cba9f62233166066901585442005e76a512853c8
jenny <dooly3392@gmail.com>
2024-02-20 22:23:24 +0900

    ApexCharts 추가 API 응답 값을 사용하여 차트 표시

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface IHistorical {
   time_open: string;
@@ -21,5 +22,54 @@ export default function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "price",
+              data: data?.map((price) => Number(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              labels: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
